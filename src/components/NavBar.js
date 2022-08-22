80df582cf56df2f6c001082af2ae4617c213f4bd
Alvarez Axel <gabriel8037.ga@gmail.com>
2022-08-22 20:24:10 -0300

    components

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+const NavBar = () => {
+    return ( 
+        <header class="NavBarJS">
+  <nav>
+  <h2>Dermatologia</h2>
+    <ul>
+      <li>
+        <a href="https://github.com/alvarez8037/ReactJs">inicio</a>
+      </li>
+      <li>
+        <a href="https://github.com/alvarez8037/ReactJs">servicios</a>
+      </li>
+      <li>
+        <a href="https://github.com/alvarez8037/ReactJs">contacto</a>
+      </li>
+      <li>
+        <a href="https://github.com/alvarez8037/ReactJs">tienda</a>
+      </li>
+      <li>
+        <a href="https://github.com/alvarez8037/ReactJs">galeria</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+    )
+}
+
+export default NavBar
